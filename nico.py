b44d85ba1116f49b995d523d4f75173708e41630
Nicoleus1 <55134353+nicoleus@users.noreply.github.com>
2019-09-26 12:20:30 +0700

    nico.py

---
+++ nico.py
@@ -0,0 +1,34 @@
+# About:
+Multi-bruteforce-facebook
+
+Author Pirmansx
+
+BATAKCYBER  team
+
+Uploaded & translated by NICOLEUS F SITORUS
+
+
+# installation & usage:
+
+apt update
+
+apt upgrade -y
+
+pkg install python 2 -y
+
+pkg install git -y
+
+https://github.com/nicoleus/mekkel
+
+pip2 install mechanize
+
+cd nico
+
+python2 nico.py
+
+# Disclaimer:
+I just want to say that do not use this tool for illegal purpose
+
+I'm not responsible for your actions.
+
+Enjoy :)
